8509c7d2691e8a64fadf36f31647100507b5f954
Juan Morales <jgamesterror@gmail.com>
2022-12-17 20:55:04 -0500

    quite el middleware origin

--- dist/middlewares/validate-origin.js
+++ dist/middlewares/validate-origin.js
@@ -3,14 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.validationOrigin = void 0;
 const whiteList = [process.env.FRONTEND_URL];
 const validationOrigin = (req, res, next) => {
-    if (whiteList.includes(process.env.FRONTEND_URL)) {
-        next();
-    }
-    else {
-        return res.status(400).json({
-            ok: false,
-            message: 'No se puede realizar la petición',
-        });
-    }
+    // if (whiteList.includes(process.env.FRONTEND_URL as string)) {
+    // 	next();
+    // } else {
+    // 	return res.status(400).json({
+    // 		ok: false,
+    // 		message: 'No se puede realizar la petición',
+    // 	});
+    // }
+    next();
 };
 exports.validationOrigin = validationOrigin;
